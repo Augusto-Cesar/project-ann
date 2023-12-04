ac8ea544bbef7a8e928748cad4c61772fa9d5f31
Augusto Cesar <cesargt92@gmail.com>
2023-12-04 00:32:34 -0300

    Ajustes no cors para permitir post

--- src/main/java/com/ann/annsistem/AnnsistemApplication.java
+++ src/main/java/com/ann/annsistem/AnnsistemApplication.java
@@ -2,12 +2,25 @@ package com.ann.annsistem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AnnsistemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnnsistemApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
